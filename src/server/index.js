81c256d8ca615a7e85e819bf3e767be954867b8a
Noman-sheikh1 <maanb645@gmail.com>
2025-10-02 20:51:07 +0530

    making log file in http method

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,55 @@
 const http = require("http");
-const url=require("url");
+const fs = require("fs");
+const url = require("url");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Welcome to the Home Page!");
-  } else if (req.url === "/about") {
-    res.end("This is the About Page.");
-  } else if (req.url === "/api") {
-    res.setHeader("Content-Type", "application/json"); 
-    res.end(JSON.stringify({ message: "Hello from API", status: "success" }));
-  } else {
-    res.statusCode = 404; 
-    res.end("404 Page Not Found");
-  }
+const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
+  
+
+const now = new Date();
+const day = String(now.getDate()).padStart(2, "0");      // 02
+const month = String(now.getMonth() + 1).padStart(2, "0"); // 10 (months start from 0)
+const year = now.getFullYear();                         // 2025
+
+const formattedDate = `${day}-${month}-${year}`;
+
+
+
+  const log = `${formattedDate} : ${req.method} ${req.url} New Req Received\n`;
+  const myUrl = url.parse(req.url, true);
+
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (myUrl.pathname) {
+      case "/":
+        if (req.method === "GET") res.end("HomePage");
+        break;
+
+      case "/about":
+        const username = myUrl.query.myname;
+        res.end(`Hi, ${username}`);
+        break;
+
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end("Here are your results for " + search);
+        break;
+
+      case "/signup":
+        if (req.method === "GET") {
+          res.end("This is a signup Form");
+        } else if (req.method === "POST") {
+          // DB Query can be added here
+          res.end("Success");
+        }
+        break;
+
+      default:
+        res.end("404 Not Found");
+    }
+  });
 });
 
-server.listen(3000, () => {
-  console.log("Server is running at http://localhost:3000/");
-});
+myServer.listen(3000, () => console.log("Server started at port 3000"));
 
 
 
