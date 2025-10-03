301f607a53840678bb85f47883c6f401dbda9ed7
Noman-sheikh1 <maanb645@gmail.com>
2025-10-03 21:05:08 +0530

    adding usage os postman testing tool and post patch delete api

--- src/project_1/index.js
+++ src/project_1/index.js
@@ -1,7 +1,11 @@
 const express=require("express");
 const users=require("./MOCK_DATA.json");
 const app=express();
+const fs=require("fs");
 const PORT=8000;
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json()); 
 
  app.get("/api/users",(req,res)=>{
     return res.json(users);
@@ -13,13 +17,46 @@ const PORT=8000;
     return res.json(user);
  })
  app.post("/api/users",(req,res)=>{
-    res.json({status:"pending"})
+    const body=req.body;
+  
+    users.push({...body,id:users.length+1});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users,null,2),(err,data)=>{
+        return  res.json({status:"sucess",id:users.length})
+    })
+   
  })
  app.patch("/api/users/:id",(req,res)=>{
-    res.json({status:"pending"})
+    const  body=req.body;
+    console.log(body);
+    const id=Number(req.params.id);
+    const userIndex=users.findIndex((user)=>user.id==id);
+    if(userIndex===-1){
+        res.status(404).json({message:"user not found"});
+    }
+   users[userIndex] = { ...users[userIndex], ...body };
+   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
+    if (err) return res.status(500).json({ error: "Error writing file" });
+    res.json({ status: "success", user: users[userIndex] });
+  });
  })
- app.delete("/api/users/:id",(req,res)=>{
-    res.json({status:"pending"})
- })
+ app.delete("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const userIndex = users.findIndex((user) => user.id === id);
+
+  if (userIndex === -1) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  // Remove user
+  const deletedUser = users.splice(userIndex, 1);
+
+  // Save back to file
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
+    if (err) return res.status(500).json({ error: "Error writing file" });
+    res.json({ status: "success", deletedUser });
+  });
+});
+
 
  app.listen(PORT,()=>console.log("server started at port :",PORT));
